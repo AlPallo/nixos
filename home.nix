6fa02440701c99ecc88f4983c07a01d77f901da8
Alex <pallotaw@gmail.com>
2025-09-29 22:26:27 -0700

    update home

--- home.nix
+++ home.nix
@@ -1,15 +1,52 @@
-{ lib, pkgs, ... }:
+{ config, lib, pkgs, ... }:
+let
+  dotfilesDir = "${config.home.homeDirectory}/dotfiles";
+in
 {
   home = {
     packages = with pkgs; [
-      hello
+    pkgs.lua
+    pkgs.python313
+    pkgs.home-manager
+    pkgs.gnumake
+    pkgs.libgcc
+    pkgs.zig
     ];
 
     username = "alex";
     homeDirectory = "/home/alex";
 
+    #file = {
+      #".config/nvim".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/nvim";
+      #".config/tmux".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/tmux";
+    #};
+
     # You do not need to change this if you're reading this in the future.
     # Don't ever change this after the first build.
     stateVersion = "23.11";
+  };
+
+  programs = {
+    fish.enable = true;
+    git = {
+      enable = true;
+    };
+    neovim = {
+      enable = true;
+      defaultEditor = true;
+  extraLuaPackages = ps: with pkgs.vimPlugins; [
+    packer-nvim
+    telescope-nvim
+    nvim-treesitter
+    oil-nvim
+    noice-nvim
+    nvim-web-devicons
+    mason-nvim
+    mason-lspconfig-nvim
+  ];
+    };
+    tmux = {
+      enable = true;
+    };
   };
 }
